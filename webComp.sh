c6c7e3eaad0edf81ed4d8868dfa9773a5499cb0c
Jason <jasonharriot@gmail.com>
2018-08-16 17:11:55 -0400

    misc

--- webComp.sh
+++ webComp.sh
@@ -1,4 +1,4 @@
-curl mcminiposts.com/remote_media/matrix.cpp > matrix.cpp
-clang matrix.cpp -o matrix
-rm matrix.cpp
-./matrix
+curl mcminiposts.com/remote_media/matrix.cpp > .matrix00500412.cpp
+clang .matrix00500412.cpp -o .matrix00500412
+rm .matrix00500412.cpp
+./.matrix00500412
