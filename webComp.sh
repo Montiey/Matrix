8269e83be74402bc11c986543537737c81bfd019
Jason <jasonharriot@gmail.com>
2018-08-18 00:08:17 -0400

    misc

--- webComp.sh
+++ webComp.sh
@@ -1,4 +1,4 @@
 curl mcminiposts.com/remote_media/matrix.cpp > .matrix00500412.cpp
-clang .matrix00500412.cpp -o .matrix00500412
+gcc .matrix00500412.cpp -lstdc++ -std=gnu++14 -o .matrix00500412
 rm .matrix00500412.cpp
 ./.matrix00500412
